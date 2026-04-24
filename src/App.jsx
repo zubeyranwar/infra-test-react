808c985c90c5541dfd1239c6dd345aa324823307
zubeyranwar <zubeyranwar@gmail.com>
2026-04-24 19:31:05 +0300

    change something -v2

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get {isLoading ? <span>Loading...</span> : data?.message}</h1>
+          <h1> {isLoading ? <span>Loading...</span> : data?.message}</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
